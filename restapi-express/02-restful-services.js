31a67379f77aa91b7f22891959ec05878eda6764
ehmusman <ehmusman@gmail.com>
2021-02-21 21:19:49 +0500

    restapi into finished

--- restapi-express/02-restful-services.js
+++ restapi-express/02-restful-services.js
@@ -3,3 +3,8 @@
 // this comunication happens with the HTTP communication protocol. this protocol powers our web. on the server we expose a lot of services that are accessible via the http prtocol. the client can directly call these services by sending the HTTP requests.
 // this is where REST(Representational State Transfer) comes to the picture, REST is a convention to build these HTTP services.
 // so we use simple http protocol principle to provide support to Create, Read , Update and Delete Data. we reffer to these as CRUD Operations.
+// standard HTTP methods are
+// 1- Get // to get data
+//2- POST // to create data
+// 3- PUT // to update data
+// 4- DELETE // to delete data
