c891fa0f662c75d19b23c3a4cb3e3c3948f7863b
ehmusman <ehmusman@gmail.com>
2021-02-21 14:40:39 +0500

    view package info from repo and by using command is done

---
+++ node-package-manager/05-view-repo-info-for-pkg.js
@@ -0,0 +1,8 @@
+// if we want to learn about npm package. its versions, all matadata about the specifically mongoose package. one of the interesting properties like dependencies of that package can be seen from npmjs.com  repository. but there is a fastest way to know about all the metadata of a package.
+// run command 'npm view package-name'
+// in my case 'npm view mongoose'
+// to view only dependencies run 
+// npm view mongoose dependencies
+// if we want to see all the version of a package run this
+// npm view mongoose versions
+// this is helpfull when ve have to upgrade or downgrade the specific version
