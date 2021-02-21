5e3cf4f8d2044a4e29206c530dc377032cd0b422
ehmusman <ehmusman@gmail.com>
2021-02-21 14:45:36 +0500

    specific version of mongoose is installed

--- node-package-manager/06-installing-specific-version.js
+++ node-package-manager/06-installing-specific-version.js
@@ -1,1 +1,4 @@
-// ssome time
+// ssome time we have to install the specific version of a specific version than this can be doneby using the following comand
+// npm install mongoose@version
+//version should be with its Major Miner and Patch releases.
+// if we didnt write the version name then automatically the latest version will be downloaded 
