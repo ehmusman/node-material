02f11208a1ab5fc74472a92e5f65ad9caf78395a
ehmusman <ehmusman@gmail.com>
2021-02-21 20:12:03 +0500

    global packages are also covered

---
+++ node-package-manager/10-global-pkg.js
@@ -0,0 +1,6 @@
+// all the packages that we have yet installed are not global packages. they are only accessible from the from where they have installed. some packages are also global packages. they can be accessible from any folder. they are called global packages. npm is its example.
+// npm is a command line tool which can be running from any where.
+// we use -g flag to install a package globaly.
+// to list the global packages we use 'npm list -g'
+// npm -g outdated // used to check the outdated versions.
+// npm -g updat // used to update the globall packages
