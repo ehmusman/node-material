f0e564570553b052e9e1ac2143d85f682b845664
ehmusman <ehmusman@gmail.com>
2021-02-21 21:52:18 +0500

    intro to express is done

---
+++ restapi-express/03-intro-express.js
@@ -0,0 +1,24 @@
+
+// this is a code of http module. we have discussed about it earlier.
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write("Hello World")
+        res.end();
+    }
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3, 4, 5]))
+        res.end();
+
+    }
+})
+
+server.listen(3000)
+
+console.log('listening on port 3000......')
+
+// this approach is not useable in real life. because if we ad more routes in our app the if else statements are increased and complaxity of app is also increased.
+// at this point a framework appear in the picture called Express.
+// this framework give our application a propper structure. so we can add more routes in our application while keeping the application code maintainable.
+// there are various framewoks to create web application on top of node. but hte most populer one is express.
