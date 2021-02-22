0df2dd8bed230b77951a1be99e16e647199a83ea
ehmusman <ehmusman@gmail.com>
2021-02-22 14:35:29 +0500

    get and post requests are done

--- vidly/movies-genres.js
+++ vidly/movies-genres.js
@@ -37,11 +37,8 @@ app.get('/api/genres/:id', (req, res) => {
 
 app.post('/api/genres', (req, res) => {
 
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
-    const result = schema.validate(req.body)
-    if (result.error) return res.status(400).send(result.error.details[0].message)
+    const { error } = validation(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 
     const genre = {
         id: genres.length + 1,
@@ -51,6 +48,41 @@ app.post('/api/genres', (req, res) => {
     res.send(genre)
 })
 
+app.put('/api/genres/:id', (req, res) => {
+    // check the id if it exists
+    const genre = genres.find(c => c.id === parseInt(req.params.id))
+    if (!genre) return res.status(400).send("The Cours with the given id is not Present")
+
+    // if exists but hte post condition is not satisfied than send the error of 400;
+    const { error } = validation(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    // update the genre
+    genre.name = req.body.name;
+
+    // return the updated genre
+    res.send(genre)
+})
+
+app.delete('/api/genres/:id', (req, res) => {
+    // check the id if it exists
+    const genre = genres.find(c => c.id === parseInt(req.params.id))
+    if (!genre) return res.status(400).send("The Cours with the given id is not Present")
+
+    // Delete
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1)
+
+    // Return the same genre
+    res.send(genre);
+})
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Listening on port ${port} ....`))
+
+const validation = (course) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    return schema.validate(course)
+}
