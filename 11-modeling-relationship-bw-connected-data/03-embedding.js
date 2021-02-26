bffcda44a05e0f9c1d29c8e65ff0e15069e2a6d8
ehmusman <ehmusman@gmail.com>
2021-02-26 23:36:41 +0500

    embedding is finished

--- 11-modeling-relationship-bw-connected-data/03-embedding.js
+++ 11-modeling-relationship-bw-connected-data/03-embedding.js
@@ -13,7 +13,11 @@ const authorSchema = new mongoose.Schema({
 const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
-    name: String
+    name: String,
+    author: {
+        type: authorSchema,
+        required: true
+    }
 }));
 
 async function createCourse(name, author) {
@@ -31,4 +35,25 @@ async function listCourses() {
     console.log(courses);
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+
+// createCourse('Node Course', new Author({ name: 'Usman' }));
+// now we have author is an embedded or subdocument with properties name and id. rhese subdocuments are like normal documents. so most of the features thar are available in normal documents are also available in sub documents. for example we can do validation here. like author.name should be required. but these documents cannot be saved under own, but these can be saved under the context of its parent. so now change the name of this author
+
+async function updateAuthor(courseId) {
+    // const course = await Course.findById(courseId);
+    // course.author.name = "Usman Bakhsh"
+    // course.save();
+    // we can also update the subdocument directly 
+    // so instead of querrying it first we can updated directly in the database. so now modify this course
+
+    const course = await Course.update({ _id: courseId }, {
+        // $set: {
+        //     'author.name': "Ehsan Bakhsh"
+        // }
+        $unset: {
+            'author': ''
+        }
+    })// so now we update it directly in the database
+    // to remove the subdocument we can use the unset operator
+}
+updateAuthor("60393cce81d0655c37bffc55")
