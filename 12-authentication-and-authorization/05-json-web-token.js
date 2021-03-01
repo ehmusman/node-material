280047951fe971a4e3edfca3048146b31277755e
ehmusman <ehmusman@gmail.com>
2021-03-01 15:49:48 +0500

    json web token is started

---
+++ 12-authentication-and-authorization/05-json-web-token.js
@@ -0,0 +1,3 @@
+// in login method we send simple true response like res.send(true) on valid authentication. but in real application we dont send only true value.
+// we send Json web tokens in response
+// json webtoken is a long string that is actually identified a user. as a metaphor we can think it like a driving liscence or a passport.
