f26800196e1dbe7fd846dcbc1838bd28d6dc1da5
ehmusman <ehmusman@gmail.com>
2021-03-03 10:39:18 +0500

    log to mongoDB covered

---
+++ 13-handeling-and-logging-errors/06-log-to-mongodb.js
@@ -0,0 +1,9 @@
+// npm i winston-mongodb@3.0.0
+//in index.js
+// require(winston-mongodb)
+// winston.add(winston.transports.MongoDB, {db: 'mongodb://localhost/vidly'})
+
+// add this line in genres get route handler
+// throw new Error("Could not get genres..")
+
+// on the request the a log collection will be created in the mongodb
