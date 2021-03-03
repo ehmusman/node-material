6de0bb107d153a55830fee8e57bbd5e439af094a
ehmusman <ehmusman@gmail.com>
2021-03-03 11:48:49 +0500

    new winston.transports.Console({ colorize: true, prettyPrint: true }), error is also consoled for helping the developers

--- vidly/startup/logging.js
+++ vidly/startup/logging.js
@@ -3,6 +3,7 @@ require('winston-mongodb')
 const winston = require("winston")
 module.exports = function () {
     winston.handleExceptions(
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
         new winston.transports.File({ filename: 'uncaughtExceptions.log' })
     )
     process.on('unhandledRejection', ex => {
